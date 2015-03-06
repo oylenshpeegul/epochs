253cc708907f12a2681c9200652ff78b04067f13
oylenshpeegul <theaney@gmail.com>
2015-03-06 18:53:11 -0500

    combine tests

--- epochs_test.go
+++ epochs_test.go
@@ -5,43 +5,38 @@ import (
 	"time"
 )
 
-var ChromeTests = []struct {
+var MixedTests = []struct {
+	f   func(int64) time.Time
 	num int64
 	exp time.Time
 }{
-	{12879041490000000,
-		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC)},
-	{12912187816559001,
+	{
+		Chrome,
+		12879041490000000,
+		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC),
+	},
+	{
+		Chrome,
+		12912187816559001,
 		time.Date(2010, time.March, 4, 14, 50, 16, 559001000, time.UTC)},
+	{
+		Unix,
+		1234567890,
+		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC),
+	},
+	{
+		Unix,
+		-1234567890,
+		time.Date(1930, time.November, 18, 0, 28, 30, 0, time.UTC),
+	},
 }
 
-func TestChrome(t *testing.T) {
+func TestMixed(t *testing.T) {
+	for _, tt := range MixedTests {
 
-	for _, tt := range ChromeTests {
-
-		obs := Chrome(tt.num)
+		obs := tt.f(tt.num)
 		if obs != tt.exp {
-			t.Errorf("Unix(%q) => %q, want %q", tt.num, obs, tt.exp)
-		}
-	}
-}
-
-var UnixTests = []struct {
-	num int64
-	exp time.Time
-}{
-	{1234567890,
-		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC)},
-	{-1234567890,
-		time.Date(1930, time.November, 18, 0, 28, 30, 0, time.UTC)},
-}
-
-func TestUnix(t *testing.T) {
-	for _, tt := range UnixTests {
-
-		obs := Unix(tt.num)
-		if obs != tt.exp {
-			t.Errorf("Unix(%q) => %q, want %q", tt.num, obs, tt.exp)
+			t.Errorf("%q(%q) => %q, want %q", tt.f, tt.num, obs, tt.exp)
 		}
 	}
 }
