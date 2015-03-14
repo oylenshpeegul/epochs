441f0b4e37e473d8687548e46a5c739d8d5a2184
oylenshpeegul <theaney@gmail.com>
2015-03-14 11:12:34 -0400

    add tests for GoogleCalendar

--- epochs_test.go
+++ epochs_test.go
@@ -31,6 +31,16 @@ var MixedTests = []struct {
 		time.Date(2010, time.December, 17, 0, 23, 53, 0, time.UTC),
 	},
 	{
+		GoogleCalendar,
+		1297899090,
+		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC),
+	},
+	{
+		GoogleCalendar,
+		1234567890,
+		time.Date(2007, time.March, 16, 23, 31, 30, 0, time.UTC),
+	},
+	{
 		Java,
 		1234567890000,
 		time.Date(2009, time.February, 13, 23, 31, 30, 0, time.UTC),
