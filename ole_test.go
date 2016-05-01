c1e61005e5fb1f775d240bf3246320cd334eaf87
oylenshpeegul <theaney@gmail.com>
2016-05-01 14:48:45 -0400

    reverse tests

--- ole_test.go
+++ ole_test.go
@@ -30,3 +30,29 @@ func TestOLE(t *testing.T) {
 		}
 	}
 }
+
+var ToOLETests = []struct {
+	f   func(time.Time) string
+	t   time.Time
+	exp string
+}{
+	{
+		ToOLE,
+		time.Date(2009, time.February, 13, 23, 31, 30, 83, time.UTC),
+		"dedddd5d3f76e340",
+	},
+	{
+		ToOLE,
+		time.Date(2011, time.February, 23, 21, 31, 43, 127000061, time.UTC),
+		"8ad371b4bcd2e340",
+	},
+}
+
+func TestToOLE(t *testing.T) {
+	for _, tt := range ToOLETests {
+		obs := tt.f(tt.t)
+		if obs != tt.exp {
+			t.Errorf("%q(%q) => %q, want %q", tt.f, tt.t, obs, tt.exp)
+		}
+	}
+}
